06e1af74660280d78541919d80f9336a567ae396
nutv99 <nutv99@gmail.com>
2022-09-06 13:11:57 +0700

    2 files modified

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-panel',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-panel.component.css']
 })
 export class SidePanelComponent implements OnInit {
+ @Input sCaption: string = '' ;  
+ 
+panelWidth : number  = 10 ;
 
   constructor() { }
 
