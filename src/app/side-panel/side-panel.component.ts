a76f363e2d07b5a3b3a9814f38725c9fe24dcb89
nutv99 <nutv99@gmail.com>
2022-09-06 13:35:02 +0700

    3 files modified

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -1,18 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input,ViewChild,ElementRef, OnInit,AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
   styleUrls: ['./side-panel.component.css']
 })
-export class SidePanelComponent implements OnInit {
+export class SidePanelComponent implements AfterViewInit {
  @Input() sCaption: string = '' ;  
-
-panelWidth : number  = 10 ;
+ @ViewChild("myPanel") myPanel: ElementRef;
+panelWidth : number  = 200 ;
 
   constructor() { }
 
   ngOnInit() {
+    this.myPanel.nativeElement.innerHTML = "I am changed by ElementRef & ViewChild";
+  }
+
+  ngAfterViewInit() {
+    this.myPanel.nativeElement.innerHTML = "I am changed by ElementRef & ViewChild";
+   // this.myPanel.nativeElement.style.width= '600px'
+
+
   }
 
 }
