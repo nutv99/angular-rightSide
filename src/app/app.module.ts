6dfd6113463144f478762613dc646b672d97592b
nutv99 <nutv99@gmail.com>
2022-09-06 16:39:57 +0700

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SidePanelComponent } from './side-panel/side-panel.component' ;
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,HttpClientModule ],
   declarations: [ AppComponent, HelloComponent,SidePanelComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -9,28 +9,35 @@ import {
   EventEmitter,
   AfterViewInit,
   SimpleChanges,
-} from '@angular/core' ; 
-
+} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
   styleUrls: ['./side-panel.component.css'],
 })
-
-export class SidePanelComponent implements OnInit,AfterViewInit {
-
+export class SidePanelComponent implements OnInit, AfterViewInit {
   @Input() sCaption: string = '';
-  @Input() panelWidth: string ='';
-  @Input() dataCode: string ='';
+  @Input() panelWidth: string = '';
+  @Input() dataCode: string = '';
+
+  dataResult: any = {};
 
   @ViewChild('myPanel') myPanel: ElementRef;
   @Output() outputPanelWidth = new EventEmitter<string>();
 
-  constructor() {}
+  constructor(private myhttp: HttpClient) {}
 
   ngOnInit() {
-    
+    this.myhttp
+      .get<any>('https://lovetoshopmall.com/dataservice/categoryTest.php')
+      .subscribe((result) => {
+        this.dataResult = result;
+        console.log(result.value);
+      });
   }
 
   ngAfterViewInit() {
@@ -39,16 +46,14 @@ export class SidePanelComponent implements OnInit,AfterViewInit {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    
     // this.myPanel.nativeElement.style.width = this.panelWidth ;
     //this.myPanel.nativeElement.style.width = '350px' ;
-     
   }
 
   ClosePanel() {
     //this.myPanel.nativeElement.classList.add('otherclass');
-    let sValue = '0px' ;
-    this.panelWidth = '0px;' ;
+    let sValue = '0px';
+    this.panelWidth = '0px;';
     this.outputPanelWidth.emit('0px');
     //this.myPanel.nativeElement.style.width= '10px'
     //this.myPanel.nativeElement.classList.add("otherclass");
