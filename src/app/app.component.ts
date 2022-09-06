dc3a8baef4f6aaac936ccb8ea8d1e6749c8801d4
nutv99 <nutv99@gmail.com>
2022-09-06 13:09:30 +0700

    3 files modified, 3 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component, VERSION } from '@angular/core';
+import { SidePanelComponent } from './side-panel/side-panel.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  OpenRightSide() {
+
+    
+  }
 }
